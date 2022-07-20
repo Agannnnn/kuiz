a2fbb09e8d97631d606de935b6dd6e718edf5980
Agannnnn <manusia0.5gabus@gmail.com>
2022-07-20 12:19:46 +0700

    A vew modifying

--- pages/selesai/index.vue
+++ pages/selesai/index.vue
@@ -7,6 +7,7 @@ import {
   query,
   where,
 } from "@firebase/firestore";
+import { defineComponent } from "vue";
 import { auth, db } from "../../firebase/config";
 
 interface JawabanKuiz {
@@ -27,12 +28,13 @@ interface Data {
   nilai: number;
 }
 
-export default {
+export default defineComponent({
   data(): { kuiz: Data[] } {
     return {
       kuiz: [],
     };
   },
+  fetchOnServer: false,
   async fetch() {
     const data = await getDocs(
       query(
@@ -40,27 +42,38 @@ export default {
         where("user", "==", auth.currentUser?.uid)
       )
     );
+    const kuiz: Data[] = [];
     data.forEach(async (data) => {
-      const kuiz = (
+      const docKuiz = (
         await getDoc(doc(db, `kuiz/${(data.data() as JawabanKuiz).idKuiz}`))
       ).data() as Kuiz;
 
-      const jawabanKuiz = { judul: kuiz.judul, nilai: 0 };
+      const jawabanKuiz = { judul: docKuiz.judul, nilai: 0 };
       (data.data() as JawabanKuiz).jawaban.forEach((jawaban, i) => {
-        if (jawaban === kuiz.soal[i].jawabanBenar) {
-          jawabanKuiz.nilai += 100 / kuiz.soal.length;
+        if (jawaban === docKuiz.soal[i].jawabanBenar) {
+          jawabanKuiz.nilai += 100 / docKuiz.soal.length;
         }
       });
 
-      this.kuiz.push(jawabanKuiz);
+      kuiz.push(jawabanKuiz);
     });
+    this.kuiz = kuiz;
   },
-};
+});
 </script>
 
 <template>
   <div>
     <h1>Daftar Kuiz Selesai</h1>
+    <button
+      v-if="!$fetchState.pending"
+      @click="$fetch"
+      class="tombol-muat-ulang"
+    >
+      Muat ulang
+    </button>
+    <span v-if="$fetchState.pending">Sedang memuat data...</span>
+    <span v-if="$fetchState.error">Gagal memuat data...</span>
     <div class="kuiz">
       <span>No.</span>
       <span>Judul</span>
@@ -89,6 +102,19 @@ export default {
     padding: 2px 5px;
     border-radius: 4px;
     background-color: red;
+  }
+}
+.tombol-muat-ulang {
+  border: 3px solid rgb(50, 50, 50);
+  background-color: #ffffff;
+  padding: 5px 1em;
+  margin-bottom: 0.5em;
+  cursor: pointer;
+  display: block;
+
+  &:is(:hover, :active, :focus) {
+    color: #ffffff;
+    background-color: rgb(50, 50, 50);
   }
 }
 </style>
